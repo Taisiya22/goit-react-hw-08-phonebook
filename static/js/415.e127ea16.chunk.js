0dda25060edb6eb5b60b6566947d6a5c0710e54f
Taisiya22 <110686609+Taisiya22@users.noreply.github.com>
2023-04-29 08:12:56 +0000

    Deploying to gh-pages from @ Taisiya22/goit-react-hw-08-phonebook@f97475843d41e1ed76a85b83e5ab4353e56b235c 🚀

---
+++ static/js/415.e127ea16.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184);function o(){return(0,r.jsx)("div",{children:(0,r.jsxs)("h1",{style:{textAlign:"center",marginTop:"200px",fontStyle:"italic",color:"#1976D2"},children:["Welcome to your phonebook",(0,r.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=415.e127ea16.chunk.js.map
