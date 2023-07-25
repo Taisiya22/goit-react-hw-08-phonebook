7d6c25e4564e7edfd7e4b79aa05cfc1ad64f35be
Taisiya Tomashevska <taya22011985@gmail.com>
2023-07-25 14:16:18 +0300

    ADDED: title and favicon

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -5,55 +5,58 @@ import { Button } from '@mui/material';
 import { register } from 'redux/auth/authOperations';
 import css from './RegisterForm.module.css';
 
+
 export const RegisterForm = () => {
   const dispatch = useDispatch();
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      const form = e.currentTarget;
+      dispatch(
+        register({
+          name: form.elements.name.value,
+          email: form.elements.email.value,
+          password: form.elements.password.value,
+        })
+      );
+      form.reset();
+    };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
+
+
+    return (
+      <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          type="text"
+          name="name"
+          size="small"
+          required
+        />
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          type="email"
+          name="email"
+          size="small"
+          required
+        />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          type="password"
+          name="password"
+          size="small"
+          required
+        />
+        <Button variant="contained" type="submit" size="small">
+          Register
+        </Button>
+      </form>
     );
-    form.reset();
-  };
+  }
 
-
-  return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <TextField
-        id="outlined-basic"
-        label="Username"
-        variant="outlined"
-        type="text"
-        name="name"
-        size="small"
-        required
-      />
-      <TextField
-        id="outlined-basic"
-        label="Email"
-        variant="outlined"
-        type="email"
-        name="email"
-        size="small"
-        required
-      />
-      <TextField
-        id="outlined-basic"
-        label="Password"
-        variant="outlined"
-        type="password"
-        name="password"
-        size="small"
-        required
-      />
-      <Button variant="contained" type="submit" size="small">
-        Register
-      </Button>
-    </form>
-  );
-};
